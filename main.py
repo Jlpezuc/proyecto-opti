f22912253184911d25c4cb6f1fdfdd20f7b35fbd
Sebastián <s.castroyang@uc.cl>
2022-10-13 22:26:32 -0300

    conjuntos definidos

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ model = Model()
 model.setParam("TimeLimit", 1800)  # Tiempo máximo en segundos
 
 # ---------------- Conjuntos ------------------#
+
+S_ = range(1, 38 + 1)  # Semanas en 1 año escolar
+T_ = range(1, 5 + 1)  # Días de clases en 1 semana
+E_ = range(6, 18 + 1)  # Edades
+I_ = range(1, 137 + 1)  # Alimentos que ofrece la Junaeb
+J_ = range(1, 9931 + 1)  # Numero colegios
+N_ = range(1, 3 + 1)  # Tipos de macronutrientes
+D_ = range(1, 3 + 1)  # Tipos de dietas
