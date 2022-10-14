0b9c6985caf6350a7c2069c31c651a3f272add8d
Sebastián <s.castroyang@uc.cl>
2022-10-13 23:02:57 -0300

    parametros

--- main.py
+++ main.py
@@ -15,6 +15,36 @@ j_ = range(1, 9931 + 1)  # Numero colegios
 n_ = range(1, 3 + 1)  # Tipos de macronutrientes
 d_ = range(1, 3 + 1)  # Tipos de dietas
 
+# ---------------- Importar Parametros ----------#
+
+presupuesto = ""
+costo_gramo = ""
+costo_almacenamiento = ""
+min_calorias = ""
+max_calorias = ""
+min_macronutrientes = ""
+calorias_alimento = ""
+cant_nutriente_alimento = ""
+cant_max_gramos_almacenaje = ""
+masa_porcion_alimento = ""
+cant_estudiantes_instituto = ""
+
+# ---------------- Creación Parametros ----------#
+
+p = {(s): presupuesto[s - 1] for s in s_}
+cg = {(i): costo_gramo[i - 1] for i in i_}
+cc = {(i, j): costo_almacenamiento[i - 1][j - 1] for i in i_ for j in j_}
+mca = {(e): min_calorias[e - 1] for e in e_}
+MCA = {(e): max_calorias[e - 1] for e in e_}
+mn = {(n, e): min_macronutrientes[n - 1][e - 1] for n in n_ for e in e_}
+qca = {(i): calorias_alimento[i - 1] for i in i_}
+qn = {(n, i): cant_nutriente_alimento[n - 1][i - 1] for n in n_ for i in i_}
+qc = {(j): cant_max_gramos_almacenaje[j - 1] for j in j_}
+g = {(i): masa_porcion_alimento[i - 1] for i in i_}
+qp = {(j, d): cant_estudiantes_instituto[j - 1][d - 1] for j in j_ for d in d_}
+a = ""  # agregar valor
+M = 1000000
+
 # ---------------- Variables ------------------ #
 
 # Presupuesto destinado al colegio j para la semana s
